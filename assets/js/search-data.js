54bd1662d5f54d613c232c819d0909cae8655894
aibyhana <hana.ibrahim.hilmy@gmail.com>
2025-07-02 00:11:17 +0000

    Deploying to gh-pages from @ aibyhana/aibyhana.github.io@4248a92b48160a6812e4e27bdef16eadb6f10812 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -434,7 +434,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%68%61%6E%61.%69%62%72%61%68%69%6D.%68%69%6C%6D%79@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
